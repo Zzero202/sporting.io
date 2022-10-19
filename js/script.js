b4ed45f6b3b18c73033152f06ff8ffcbddf08ebf
Zzero202 <ahmedfaisaly2000@gmail.com>
2022-10-20 01:01:32 +0200

    sporting

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// $(document).ready(function(){
+//   $(window).scroll(function(){
+//   	var scroll = $(window).scrollTop();
+// 	  if (scroll > 100) {
+// 	    $("#navigation-bar").css("background" , "linear-gradient(to right,rgba(255,0,0,0.5),rgba(0,0,255,0.9))");
+//       $("#navigation-bar").removeClass("container")
+//       $("#navigation-bar").addClass("container-fluid")
+//       $("#navigation-bar").addClass("fixed-top")
+//       $("#navigation-bar").css("height","80px")
+// 	  }
+
+// 	  else{
+// 		  $("#navigation-bar").css("background" , "transparent"); 
+//       $("#navigation-bar").removeClass("container-fluid")
+//       $("#navigation-bar").addClass("container")
+//       $("#navigation-bar").removeClass("fixed-top")
+//       $("#navigation-bar").css("height","100px") 	
+// 	  }
+//   })
+// })
+
+
+
+
